940600d4403e07aee55ee91a8e71ff8ae0a18e05
Jennifer Cole <capauldi@gmail.com>
2021-11-17 04:48:54 -0800

    form submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(taskItemEl);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
